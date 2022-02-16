dea72f18bf32c41346ebb33a1b77be6b8d9daab0
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2022-02-16 22:14:30 +0100

    Add initial attributes for Asset class

--- src/perfcalc/asset.py
+++ src/perfcalc/asset.py
@@ -4,3 +4,9 @@
 # pylint: disable=R0903
 class Asset:
     """Class defines asset characteristics."""
+
+    def __init__(self, ccy, ext_id, fname):
+        """Define asset class attributes."""
+        self.ccy = ccy
+        self.ext_id = ext_id
+        self.fname = fname
--- tests/test_asset.py
+++ tests/test_asset.py
@@ -3,14 +3,18 @@
 import pytest
 from perfcalc.asset import Asset
 
+# Disable check of docstrings in methods
 # pylint: disable=C0116
 
 
 @pytest.fixture(name="new_asset")
 def fixture_new_asset():
-    new_asset = Asset()
+    fund_name = {"EN": "Equity Fund", "PL": "Fundusz Akcji"}
+    new_asset = Asset(ccy="USD", ext_id="PL123456789012", fname=fund_name)
     yield new_asset
 
 
 def test_create_asset(new_asset):
-    assert new_asset
+    assert new_asset.ccy == "USD"
+    assert new_asset.ext_id == "PL123456789012"
+    assert new_asset.fname["PL"] == "Fundusz Akcji"
