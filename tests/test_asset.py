2a1eb908bffaae7b332bf41041d59c25e4ab72f0
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2022-02-18 23:35:27 +0100

    Enhance Asset class with more tests and methods
    In particular:
    - initialisation of class attributes implemented by protected methods
    - new test for default fund name language

--- tests/test_asset.py
+++ tests/test_asset.py
@@ -3,18 +3,35 @@
 import pytest
 from perfcalc.asset import Asset
 
-# Disable check of docstrings in methods
-# pylint: disable=C0116
+# pylint: disable=C0116 - disable check of docstrings in methods
 
 
 @pytest.fixture(name="new_asset")
 def fixture_new_asset():
-    fund_name = {"EN": "Equity Fund", "PL": "Fundusz Akcji"}
-    new_asset = Asset(ccy="USD", ext_id="PL123456789012", fname=fund_name)
+    fund_dict = {"EN": "Equity Fund", "PL": "Fundusz Akcji"}
+    new_asset = Asset(
+        default_lang="PL",
+        ccy="USD",
+        ext_id="PL123456789012",
+        fund_dict=fund_dict,
+    )
     yield new_asset
 
 
 def test_create_asset(new_asset):
     assert new_asset.ccy == "USD"
     assert new_asset.ext_id == "PL123456789012"
-    assert new_asset.fname["PL"] == "Fundusz Akcji"
+    assert new_asset.fund_dict["PL"] == "Fundusz Akcji"
+    assert new_asset.default_lang == "PL"
+
+
+def test_wrong_asset_language_name():
+    with pytest.raises(ValueError):
+        fund_dict = {"EN": "Equity Fund", "PL": "Fundusz Akcji"}
+        wrong_asset = Asset(
+            default_lang="DE",
+            ccy="USD",
+            ext_id="PL123456789012",
+            fund_dict=fund_dict,
+        )
+        return wrong_asset
