651ed38295f00cf85f29e83cf5419d23dd9b65dd
Kamil Niklasinski <kamil.niklasinski@gmail.com>
2022-02-23 20:58:10 +0100

    Initial work on Asset class (#6)
    Enhance Asset class
    
    * Add initial attributes for Asset class
    * Add more tests and methods
    
    In particular:
    - initialisation of class attributes implemented by protected methods
    - new test for default fund name language

--- src/perfcalc/asset.py
+++ src/perfcalc/asset.py
@@ -4,3 +4,36 @@
 # pylint: disable=R0903
 class Asset:
     """Class defines asset characteristics."""
+
+    def __init__(self, default_lang, ccy, ext_id, fund_dict):
+        """Define asset class attributes."""
+        self._set_currency(ccy)
+        self._set_external_id(ext_id)
+        self._set_default_lang(default_lang)
+        self._set_fund_dict(fund_dict)
+
+    def _set_external_id(self, ext_id):
+        """Set external identifier."""
+        self.ext_id = ext_id
+
+    def _set_fund_dict(self, fund_dict):
+        """Set dictionary of fund names.
+
+        Raises ValueError if default fund language is in defined fund name
+        languages.
+        """
+        fund_languages = fund_dict.keys()
+        if self.default_lang not in fund_languages:
+            raise ValueError(
+                f"Defined fund languages: {fund_languages} do not match\
+                default language: {self.default_lang}"
+            )
+        self.fund_dict = fund_dict
+
+    def _set_currency(self, ccy):
+        """Set currency."""
+        self.ccy = ccy
+
+    def _set_default_lang(self, default_lang):
+        """Set default fund name language."""
+        self.default_lang = default_lang
--- tests/test_asset.py
+++ tests/test_asset.py
@@ -3,14 +3,35 @@
 import pytest
 from perfcalc.asset import Asset
 
-# pylint: disable=C0116
+# pylint: disable=C0116 - disable check of docstrings in methods
 
 
 @pytest.fixture(name="new_asset")
 def fixture_new_asset():
-    new_asset = Asset()
+    fund_dict = {"EN": "Equity Fund", "PL": "Fundusz Akcji"}
+    new_asset = Asset(
+        default_lang="PL",
+        ccy="USD",
+        ext_id="PL123456789012",
+        fund_dict=fund_dict,
+    )
     yield new_asset
 
 
 def test_create_asset(new_asset):
-    assert new_asset
+    assert new_asset.ccy == "USD"
+    assert new_asset.ext_id == "PL123456789012"
+    assert new_asset.fund_dict["PL"] == "Fundusz Akcji"
+    assert new_asset.default_lang == "PL"
+
+
+def test_wrong_asset_language_name():
+    with pytest.raises(ValueError):
+        fund_dict = {"EN": "Equity Fund", "PL": "Fundusz Akcji"}
+        wrong_asset = Asset(
+            default_lang="DE",
+            ccy="USD",
+            ext_id="PL123456789012",
+            fund_dict=fund_dict,
+        )
+        return wrong_asset
